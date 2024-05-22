d3e73f0f7a8f941a05657439cda8449458d8effc
Josel._.Patiño <168464177+J0ZB1Lgame@users.noreply.github.com>
2024-05-21 19:32:11 -0500

    Update Setlis.java

--- src/discostu/Setlis.java
+++ src/discostu/Setlis.java
@@ -1,29 +1,69 @@
 
 package discostu;
 
-public class Setlis {
+public class Setlist {
 
     public int id;
-    public int totalCanciones;
-    public Cancion [] cancion;
+    public int totalCanciones = 0;
+    public Cancion [] song;
 
-    public Setlis() {
+    public Setlist() {
     }
 
-    public Setlis (int id) {
+    public Setlist (int id) {
         this.id = id;
-        this.cancion = new Cancion [10];
+        this.song = new Cancion [10];
     }
 
-    public void agregarCancion(Cancion cancion) {
+    public void agregarCancion(Cancion cancion, int id ) {
         
+        if (totalCanciones < song.length && song[totalCanciones] == null ){  
+            song[totalCanciones] = cancion;
+            totalCanciones++;
+            if (id == 0) {
+            System.out.println("La canción: " + cancion.toString() + " a sido agregada a la lista: Nuevas canciones");                             
+            } if (id == 1){
+            System.out.println("La cancion: " + cancion.toString() + " a sido agregada a la lista: Favoritas");
+            } if (id == 2){
+            System.out.println("La cancion: " + cancion.toString() + " a sido agregada a la lista: Triste");
+            }if (id == 3){
+            System.out.println("La cancion: " + cancion.toString() + " a sido agregada a la lista: Amor");
+            }
+            
+        } else {
+            System.out.println("Playlist llena");
+        }   
+    }         
 
+    public int getId() {
+        return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    public Cancion buscarCancion() {
-        // TODO implement here
-        return null;
+    public int getTotalCanciones() {
+        return totalCanciones;
+    }
+
+    public void setTotalCanciones(int totalCanciones) {
+        this.totalCanciones = totalCanciones;
+    }
+
+    public Cancion[] getCancion() {
+        return song;
+    }
+
+    public void setCancion(Cancion[] cancion) {
+        this.song = cancion;
+    }
+
+    
+    
+    
+    public Cancion[] buscarCancion() {
+        return song;
     }
 
     public Cancion reproducirCancion() {
