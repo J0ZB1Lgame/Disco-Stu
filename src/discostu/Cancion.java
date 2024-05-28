75b1fc604ce498a73468d7874dd1f4b6d125be04
Josel._.Patiño <168464177+J0ZB1Lgame@users.noreply.github.com>
2024-05-28 11:57:01 -0500

    Update Cancion.java
    Actualización Final

--- src/discostu/Cancion.java
+++ src/discostu/Cancion.java
@@ -2,24 +2,32 @@
 package discostu;
 
 public class Cancion {
+    
+    
 
     private String nombre;
     private String artista;
     private String album;
     private String genero;
     private Double duracion;
+    private boolean enReproduccion;
+    private boolean pausada;
+    static MP3 mp3 = new MP3();
     
     public Cancion() {
     }
+    
+//------------------------------------------------------------------------------    
     
     public Cancion(String nombre, String artista, String album, String genero, Double duracion) {
         this.nombre = nombre;
         this.artista = artista;
         this.album = album;
         this.genero = genero;
-        this.duracion = duracion;
-        
+        this.duracion = duracion;      
     }
+    
+//------------------------------------------------------------------------------    
 
     public String getNombre() {
         return nombre;
@@ -60,15 +68,52 @@ public class Cancion {
     public void setDuracion(Double duracion) {
         this.duracion = duracion;
     }
+    
+//------------------------------------------------------------------------------    
+    
     public String consultarNombre() {
-        return "";
+        return nombre;
+    }
+    
+//------------------------------------------------------------------------------    
+
+    public void iniciarReproduccion() { 
+        this.enReproduccion = true;
+        this.pausada = false;
+        System.out.println("Reproduciendo: " + nombre + " a " + mp3.volumen + "%");
     }
 
+    public void pausarReproduccion() {
+        if (enReproduccion && !pausada) {
+            this.pausada = true;
+            System.out.println("Canción pausada: " + nombre);
+        } else if (enReproduccion && pausada) {
+            this.pausada = false;
+            System.out.println("Reanudando canción: " + nombre+ " a " + mp3.volumen + "%");
+        }
+    }
+
+    public boolean estaEnReproduccion() {
+        return enReproduccion;
+    }
+
+    public boolean estaPausada() {
+        return pausada;
+    }
+
+    public void detenerReproduccion() {
+        this.enReproduccion = false;
+        this.pausada = false;
+        System.out.println("Reproducción detenida: " + nombre);
+    }
+    
+//------------------------------------------------------------------------------    
+    
     public String toString() {
        
         return "Nombre: " + nombre + " " +
                "Artista: " + artista + " " +
-               "Álbum: " + álbum + " " +
-               "Género: " + género + " " + "Duración: " + duración;
+               "Albun: " + album + " " +
+               "Genero: " + genero + " " + "Duracion: " + duracion;
     }
 }
