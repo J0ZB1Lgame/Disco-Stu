14a4ea2daa6ef194ae8b0ab5478a56f538793dcc
Josel._.Patiño <168464177+J0ZB1Lgame@users.noreply.github.com>
2024-05-28 13:27:44 -0500

    Update DiscoStu.java
    Correcciones ortográficas

--- src/discostu/DiscoStu.java
+++ src/discostu/DiscoStu.java
@@ -23,15 +23,15 @@ public class DiscoStu {
         do {
             System.out.println("--- DiscoStu ---");
             System.out.println("0. Salir");
-            System.out.println("1. Crear cancion");
+            System.out.println("1. Crear canción");
             System.out.println("2. Agregar cancion a la biblioteca");
-            System.out.println("3. lista de canciones");
+            System.out.println("3. Lista de canciones");
             System.out.println("4. Reproducir, Pausar y reanudar la reproduccion");
             System.out.println("5. Avanzar y retroceder la cancion");
             System.out.println("6. Controlar el volumen");
-            System.out.println("7. Buscar cancion por nombre");
-            System.out.println("8. Eliminar cancion");
-            System.out.println("Ingrese la opcion: ");
+            System.out.println("7. Buscar canción por nombre");
+            System.out.println("8. Eliminar canción");
+            System.out.println("Ingrese la opción: ");
             opcion = Integer.parseInt(sc.nextLine());
 
             switch (opcion) {
@@ -92,11 +92,11 @@ public class DiscoStu {
     public static void crearCancion (){
         
         System.out.println("---------------------------------------------------");
-        System.out.println("Crear cancion");
-        System.out.print("Ingrese los datos separados por - en el orden nombre - artista - album - genero - duracion min.seg: ");
+        System.out.println("Crear canción");
+        System.out.print("Ingrese los datos separados en el siguiente orden: nombre - artista - álbum - género - duración - min.seg: ");
         String datos = sc.nextLine();
-        System.out.println("Digite el numero de la biblioteca donde desea guardar esta nueva cancion: ");
-        System.out.println("1. Nuveas canciones");
+        System.out.println("Digite el número de la biblioteca donde desea guardar esta nueva canción: ");
+        System.out.println("1. Nuevas canciones");
         System.out.println("2. Favoritas");
         System.out.println("3. Tristes");
         System.out.println("4. Amor");
@@ -113,7 +113,7 @@ public class DiscoStu {
             System.out.println("---------------------------------------------------");
             
         } else {
-            System.out.println("El formato del texto no es valido");
+            System.out.println("El formato del texto no es válido");
             System.out.println("---------------------------------------------------");
         }
     }
@@ -126,10 +126,10 @@ public class DiscoStu {
     public static void agregarABiblioteca () throws InterruptedException{
         
         System.out.println("---------------------------------------------------");
-        System.out.println("agregar a nueva biblioteca");
-        System.out.println("Ingrese el nombre de la cancion que desea mover: ");
+        System.out.println("agregar a una nueva biblioteca");
+        System.out.println("Ingrese el nombre de la canción que desea mover: ");
         String nombre = sc.nextLine();
-        System.out.println("Ingrese el numero de la biblioteca a la que desea mover la cancion: ");
+        System.out.println("Ingrese el número de la biblioteca a la que desea mover la canción: ");
         System.out.println("1. Nuevas canciones");
         System.out.println("2. Favoritas");
         System.out.println("3. Tristes");
@@ -150,7 +150,7 @@ public class DiscoStu {
         
         System.out.println("---------------------------------------------------");
         System.out.println("Ingrese la bibioteca a buscar");
-        System.out.println("1. Nuveas canciones");
+        System.out.println("1. Nuevas canciones");
         System.out.println("2. Favoritas");
         System.out.println("3. Tristes");
         System.out.println("4. Amor");
@@ -202,7 +202,7 @@ public class DiscoStu {
     public static void avanzarRetroceder () throws InterruptedException{
         
         System.out.println("---------------------------------------------------");
-        System.out.println("Seleccione la direccion ('avanzar' o 'retroceder'): ");
+        System.out.println("Seleccione la dirección ('avanzar' o 'retroceder'): ");
         String direccion = sc.nextLine();
         mp3.avanzarYRetrocederCancion(direccion);
         System.out.println("---------------------------------------------------");
@@ -217,8 +217,8 @@ public class DiscoStu {
     public static void controlarVolumen (){
         
         System.out.println("---------------------------------------------------");
-        System.out.println("controlar volumen");
-        System.out.println("ingrese el volumen de 0 a 100");
+        System.out.println("controlar volúmen");
+        System.out.println("ingrese el volúmen de 0 a 100");
         int volumen = Integer.parseInt(sc.nextLine());
         mp3.controlarVolumen(volumen);
         System.out.println("---------------------------------------------------");
@@ -233,22 +233,22 @@ public class DiscoStu {
     public static void buscarCancion () throws InterruptedException{
         
         System.out.println("---------------------------------------------------");
-        System.out.println("Buscar cancion por nombre");
-        System.out.println("Escriba el nombre de la cancion a buscar");
+        System.out.println("Buscar canción por nombre");
+        System.out.println("Escriba el nombre de la canción a buscar");
         String nombre = sc.nextLine();
         int lista = mp3.buscarCancion(nombre);
         switch (lista) {
                     case 0:
-                        System.out.println("La cancion " + nombre + " ha sido encontrada en la lista: Nuevas canciones");
+                        System.out.println("La canción " + nombre + " ha sido encontrada en la lista: Nuevas canciones");
                         break;
                     case 1:
-                        System.out.println("La cancion " + nombre + " ha sido encontrada en la lista: Favoritas");
+                        System.out.println("La canción " + nombre + " ha sido encontrada en la lista: Favoritas");
                         break;
                     case 2:
-                        System.out.println("La cancion " + nombre + " ha sido encontrada en la lista: Tristes");
+                        System.out.println("La canción " + nombre + " ha sido encontrada en la lista: Tristes");
                         break;
                     case 3:
-                        System.out.println("La cancion " + nombre + " ha sido encontrada en en la lista: Amor");
+                        System.out.println("La canción " + nombre + " ha sido encontrada en en la lista: Amor");
                         break;
                 }    
         System.out.println("---------------------------------------------------");
@@ -262,8 +262,8 @@ public class DiscoStu {
     
     public static void eliminarCancion () throws InterruptedException{
         System.out.println("---------------------------------------------------");
-        System.out.println("eliminar cancion por nombre");
-        System.out.println("Escriba el nombre de la cancion a eliminar");
+        System.out.println("Eliminar canción por nombre");
+        System.out.println("Escriba el nombre de la canción a eliminar");
         String nombre = sc.nextLine();
         mp3.eliminarCancion(nombre);
         System.out.println("---------------------------------------------------");
