9617419de55804c7aa562c8c9d12ed35972631e5
Josel._.Patiño <168464177+J0ZB1Lgame@users.noreply.github.com>
2024-05-28 12:58:39 -0500

    Update Setlis.java

--- src/discostu/Setlis.java
+++ src/discostu/Setlis.java
@@ -12,12 +12,23 @@ public class Setlis {
     
 //------------------------------------------------------------------------------    
 
+    /**
+     * metodo para la declaracion del tamaño del arreglo
+     * @param id es la posicion de la lista 0 = nuevas canciones, 1 = favoritas, 2 = tristes y 3 = amor
+     */
+    
     public Setlis (int id) {
         this.id = id;
         this.song = new Cancion [10];
     }
 
 //------------------------------------------------------------------------------    
+    
+    /**
+     * luego de ingresar la cancion, este metodo agrega la cancion en una posicion dentro de la lista 
+     * @param cancion los datos de la cancion a ingresar 
+     * @param id la posicion de la lista 
+     */
     
     public void agregarCancion(Cancion cancion, int id ) {
         
@@ -46,45 +57,67 @@ public class Setlis {
 
 //------------------------------------------------------------------------------    
     
+    /**
+     * id de la pocion de la cancion 
+     * @return posicion
+     */
+    
     public int getId() {
         return id;
     }
 
+    /**
+     * cambio de posicion
+     * @param id posicion de la cancion
+     */
+    
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * total de canciones dentro de la lista 
+     * @return numero de canciones posibles de la lista 
+     */
+    
     public int getTotalCanciones() {
         return totalCanciones;
     }
 
+    /**
+     * cambio de total de canciones posibles en la lista 
+     * @param totalCanciones  
+     */
+    
     public void setTotalCanciones(int totalCanciones) {
         this.totalCanciones = totalCanciones;
     }
 
+    /**
+     * datos de la cancion 
+     * @return 
+     */
+    
     public Cancion[] getCancion() {
         return song;
     }
 
+    /**
+     * cambiar datos de la cancion 
+     * @param cancion 
+     */
+    
     public void setCancion(Cancion[] cancion) {
         this.song = cancion;
     }
     
 //------------------------------------------------------------------------------    
 
-    public Cancion reproducirCancion() {
-        // TODO implement here
-        return null;
-    }
-    
-//------------------------------------------------------------------------------    
-    
-    public Cancion pausarYReanudarCancion() {
-        // TODO implement here
-        return null;
-    }
-    
-//------------------------------------------------------------------------------    
+    /**
+     * se utiliza este metodo para buscar la cancion dentro de la lista
+     * @param nombre nombre de la cancion a buscar, este nombre fue el ingresado por el usuario
+     * @return los datos de la cancion 
+     */
     
     public Cancion buscarCancion(String nombre) {
         for (int i = 0; i < song.length; i++) {
@@ -97,6 +130,12 @@ public class Setlis {
 }
 
 //------------------------------------------------------------------------------    
+    
+    /**
+     * metodo para eliminar una cancion determiada dentro de la lista
+     * @param nombre como variable para buscar y eliminar, este nombre fue el ingresado por el usuario
+     * @return datos de la cancion eliminada
+     */
     
     public Cancion eliminarCancion(String nombre) {
         for (int i = 0; i < song.length; i++) {
